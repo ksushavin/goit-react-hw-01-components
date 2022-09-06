484e4eef8dafda13169fbafdb9147197f0f0cbd5
Oksana Bulakh <ksushavin15@gmail.com>
2022-09-07 00:10:04 +0200

    додає карточку юзера

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,19 @@
+import { Profile } from './Profile/Profile';
+import user from 'user.json';
+
 export const App = () => {
   return (
     <div>
+      <Profile
+        name={user.username}
+        tag={user.tag}
+        avatar={user.avatar}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+
+      />
     </div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,41 @@
+import css from 'components/Profile/Profile.module.css';
+
+export const Profile = ({
+    name,
+    tag,
+    location,
+    avatar = "https://cdn-icons-png.flaticon.com/512/1077/1077012.png",
+    followers,
+    views,
+    likes
+}) => {
+
+  return (
+<div className={css.profile}>
+    <div className={css.description}>
+      <img
+        src={avatar}
+        alt={name}
+        className={css.avatar}
+      />
+      <p className={css.name}>{name}</p>
+      <p className={css.tag}>@{tag}</p>
+      <p className={css.location}>{location}</p>
+    </div>
+
+    <ul className={css.stats}>
+      <li className={css.stats__item}>
+        <span className={css.label}>Followers</span>
+        <span className={css.quantity}>{followers}</span>
+      </li>
+      <li className={css.stats__item}>
+        <span className={css.label}>Views</span>
+        <span className={css.quantity}>{views}</span>
+      </li>
+      <li className={css.stats__item}>
+        <span className={css.label}>Likes</span>
+        <span className={css.quantity}>{likes}</span>
+      </li>
+    </ul>
+</div>)
+}
